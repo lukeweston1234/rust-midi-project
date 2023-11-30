27ba5583ad958e94297c81695b1b3817094b4d7a
Luke Weston <lukeweston@Lukes-MacBook-Pro.local>
2023-11-29 22:09:06 -0500

    slight refactor, going to use structs soon

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,32 @@ use std::io::{stdin, stdout, Write};
 use std::thread::sleep;
 use std::time::Duration;
 
-use midir::{MidiOutput, MidiOutputPort};
+use midir::{MidiOutput, MidiOutputPort, MidiOutputConnection};
 
 fn main() {
     match run() {
-        Ok(_) => (),
-        Err(err) => println!("Error: {}", err),
+        Ok(()) => (),
+        Err(err) => println!("Error getting MIDI output: {}", err)
     }
 }
 
-fn run() -> Result<(), Box<dyn Error>> {
+fn run() -> Result<(),Box<dyn Error>>{
+    let mut output:MidiOutputConnection = get_midi_out()?;
+    play_note(&mut output, 66, 4);
+    Ok(())
+}
+
+fn play_note(output: &mut MidiOutputConnection, note: u8, duration: u64){
+    const NOTE_ON_MSG: u8 = 0x90;
+    const NOTE_OFF_MSG: u8 = 0x80;
+    const VELOCITY: u8 = 0x64;
+
+    let _ = output.send(&[NOTE_ON_MSG, note, VELOCITY]);
+    sleep(Duration::from_millis(duration * 150));
+    let _ = output.send(&[NOTE_OFF_MSG, note, VELOCITY]);
+}
+
+fn get_midi_out() -> Result<MidiOutputConnection,Box<dyn Error>>  {
     let midi_out = MidiOutput::new("My Test Output")?;
 
     // Get an output port (read from console if multiple are available)
@@ -42,35 +58,7 @@ fn run() -> Result<(), Box<dyn Error>> {
     };
 
     println!("\nOpening connection");
-    let mut conn_out = midi_out.connect(out_port, "midir-test")?;
-    println!("Connection open. Listen!");
-    {
-        // Define a new scope in which the closure `play_note` borrows conn_out, so it can be called easily
-        let mut play_note = |note: u8, duration: u64| {
-            const NOTE_ON_MSG: u8 = 0x90;
-            const NOTE_OFF_MSG: u8 = 0x80;
-            const VELOCITY: u8 = 0x64;
-            // We're ignoring errors in here
-            let _ = conn_out.send(&[NOTE_ON_MSG, note, VELOCITY]);
-            sleep(Duration::from_millis(duration * 150));
-            let _ = conn_out.send(&[NOTE_OFF_MSG, note, VELOCITY]);
-        };
-
-        sleep(Duration::from_millis(4 * 150));
-
-        play_note(66, 4);
-        play_note(65, 3);
-        play_note(63, 1);
-        play_note(61, 6);
-        play_note(59, 2);
-        play_note(58, 4);
-        play_note(56, 4);
-        play_note(54, 4);
-    }
-    sleep(Duration::from_millis(150));
-    println!("\nClosing connection");
-    // This is optional, the connection would automatically be closed as soon as it goes out of scope
-    conn_out.close();
-    println!("Connection closed");
-    Ok(())
+    let conn_out = midi_out.connect(out_port, "midir-test")?;
+    Ok(conn_out)
 }
+
